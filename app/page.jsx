c0103a7f85230418d566207e2c5b71101e00763a
Muhammad Azfar Aslam <azfarnaslam06@gmail.com>
2025-06-19 22:08:55 +0500

    Section 2 UI

--- app/page.jsx
+++ app/page.jsx
@@ -6,8 +6,8 @@ export default function Home() {
     <>
       <HomepageBanner />
       {/* Six Column Section */}
-      <div className="relative px-6 lg:px-0 py-6 lg:py-0 rounded-2xl">
-        <div className="flex items-center py-16 bg-primary-light max-w-7xl container mx-auto px-4 mt-0 lg:mt-[-50px] sm:px-6 lg:px-8 relative lg:absolute inset-0 rounded-xl">
+      <div className="relative z-10 px-6 lg:px-0 py-6 lg:py-0 rounded-2xl">
+        <div className="flex items-center py-16 bg-primary-light max-w-7xl mx-auto px-4 mt-0 lg:mt-[-50px] sm:px-6 lg:px-8 relative lg:absolute inset-0 rounded-xl">
           <div className="grid w-full grid-cols-2 sm:grid-cols-2 lg:grid-cols-6 gap-2 justify-center items-center">
             {/* Column 1 */}
             <div className="flex flex-col items-center  border-black lg:last:border-r">
@@ -60,6 +60,32 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      <section className="relative bg-primary-light pb-16 pt-30">
+        {/* White Overlay */}
+        <div className="absolute inset-0 bg-white opacity-70 pointer-events-none"></div>
+        <div className="relative max-w-7xl mx-auto text-center px-2 lg:px-16">
+          <p className="text-sm font-medium text-primary mb-2">
+            About Ubiety Sphere
+          </p>
+          <h2 className="text-3xl md:text-4xl font-semibold text-gray-900 mb-4">
+            We provide professional, dedicated, and experienced psychological
+            support
+          </h2>
+          <p className="text-base text-gray-700 mb-6 max-w-2xl mx-auto">
+            It is a trusted counseling and therapy center, staffed by
+            experienced professionals dedicated to listening, supporting, and
+            guiding you. We believe in everyone's potential to heal and grow
+            with the right care.
+          </p>
+          <a
+            href="#"
+            className="inline-block px-6 py-3 bg-primary text-white font-medium rounded-xl shadow hover:bg-primary-dark transition"
+          >
+            Learn More About Us
+          </a>
+        </div>
+      </section>
     </>
   );
 }
