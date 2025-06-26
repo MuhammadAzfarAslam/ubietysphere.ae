23de0b8884a52a81515d55c8356fa264b7c22e1b
Muhammad Azfar Aslam <muhammad.azfar@sixlogics.com>
2025-06-26 11:36:59 +0500

    Mobile Menu

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -37,7 +37,7 @@ const MenuItem = ({ item, onClick }) => {
           {item.name}
           <svg
             className={`w-4 h-4 mt-0.5  transition-transform ${
-              open ? "transform rotate-0" : "rotate-270"
+              open ? "transform rotate-0" : "rotate-270 lg:rotate-0"
             }`}
             fill="none"
             stroke="currentColor"
@@ -54,7 +54,7 @@ const MenuItem = ({ item, onClick }) => {
         <ul
           className={`${
             open ? "block" : "hidden lg:group-hover:block"
-          } lg:bg-white lg:shadow-lg lg:absolute mt-2 py-2 rounded-md w-40 z-50`}
+          } lg:bg-white lg:shadow-lg lg:absolute mt-0 py-2 rounded-md w-40 z-50`}
         >
           {item.children.map((child, index) => (
             <li key={index}>
@@ -88,19 +88,50 @@ export default function Header() {
     <header>
       <div className="block bg-primary py-2 d-none d-sm-block text-white fw-bold w-full max-w-screen">
         <div className="max-w-7xl container mx-auto">
-          <div className="row flex items-center justify-between gx-4">
-            <div className="col-auto d-none d-lg-block text-xs px-3 lg:p-0">
+          <div className="row flex items-center justify-between gx-4 px-3">
+            <div className="col-auto d-none d-lg-block text-xs lg:p-0">
               1010 Avenue, New York, NY 10018 US.
             </div>
-            <div className="col-auto">
-              <button className="hover:underline text-white px-8 rounded-md text-xs hidden lg:block">
+            <div className="col-auto flex items-center">
+              <Link
+                href="/login"
+                className="hover:underline text-white text-xs hidden lg:block pr-4 border-r-1"
+              >
                 Login
-              </button>
+              </Link>
+              <div className="relative group pl-4">
+                {/* Register Button */}
+                <button className="text-white text-xs hidden lg:block cursor-pointer hover:underline">
+                  Register
+                </button>
+
+                {/* Dropdown Menu (visible on hover) */}
+                <div className="absolute right-0 hidden group-hover:block pb-2.5 bg-white shadow-lg rounded-md mt-0 w-30 z-[9999]">
+                  <Link
+                    href="/login"
+                    className="block text-gray-700 px-4 text-sm pb-1 pt-2 hover:bg-gray-100 rounded-md"
+                  >
+                    As Parent
+                  </Link>
+                  <Link
+                    href="/login"
+                    className="block text-gray-700 px-4 text-sm py-1 hover:bg-gray-100 rounded-md"
+                  >
+                    As Seeker
+                  </Link>
+                  <Link
+                    href="/login"
+                    className="block text-gray-700 px-4 text-sm py-1 hover:bg-gray-100 rounded-md"
+                  >
+                    As Provider
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <nav className="block relative w-full max-w-screen px-4 py-3 mx-auto bg-white bg-opacity-90 sticky top-3 shadow lg:px-8 backdrop-blur-lg backdrop-saturate-150 z-[9999]">
+      <nav className="block relative w-full max-w-screen px-4 py-3 mx-auto bg-white bg-opacity-90 sticky top-3 shadow lg:px-8 backdrop-blur-lg backdrop-saturate-150 z-[9990]">
         <div className="container flex flex-wrap items-center justify-between max-w-7xl mx-auto text-slate-800">
           <Link
             href="/"
@@ -171,26 +202,45 @@ export default function Header() {
                 </svg>
               </button>
             </div>
-            <ul className="flex flex-col h-full gap-4 p-4">
-              {menu.map((item, index) => (
-                <MenuItem
-                  key={index}
-                  item={item}
-                  onClick={() => setIsMobileMenuOpen(false)}
-                />
-              ))}
-              <li className="mt-4">
-                <a
-                  href="#"
-                  className="inline-block px-6 py-1.5 bg-primary text-white font-medium rounded-[5px] shadow relative overflow-hidden group border-2 border-transparent hover:border-primary transition"
-                >
-                  <span className="relative z-10 group-hover:text-primary">
+            <div className="flex flex-col justify-between h-[calc(100vh-110px)]">
+              <ul className="flex flex-col gap-3 p-4">
+                {menu.map((item, index) => (
+                  <MenuItem
+                    key={index}
+                    item={item}
+                    onClick={() => setIsMobileMenuOpen(false)}
+                  />
+                ))}
+              </ul>
+              <ul className="flex flex-col gap-4 p-4">
+                <li className="mt-4 flex flex-col gap-1">
+                  <Link
+                    href="/login"
+                    className="inline-block text-sm underline font-medium"
+                  >
                     Login
-                  </span>
-                  <span className="absolute left-0 top-0 w-full h-full bg-secondary transform scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-500 ease-in-out"></span>
-                </a>
-              </li>
-            </ul>
+                  </Link>
+                  <Link
+                    href="/login"
+                    className="inline-block text-sm underline font-medium"
+                  >
+                    Register as Parent
+                  </Link>
+                  <Link
+                    href="/login"
+                    className="inline-block text-sm underline font-medium"
+                  >
+                    Register as Seeker
+                  </Link>
+                  <Link
+                    href="/login"
+                    className="inline-block text-sm underline font-medium"
+                  >
+                    Register as Provider
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
 
           {/* Desktop Menu */}
